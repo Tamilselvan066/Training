6951648989aebc3f18b43608c34c253de4d3657c
Tamilselvan066 <52317515+Tamilselvan066@users.noreply.github.com>
2019-07-03 14:52:10 +0530

    Update vowelorconsonant.py

--- vowelorconsonant.py
+++ vowelorconsonant.py
@@ -1,5 +1,5 @@
 ch=input("")
 if(ch=='a' or ch=='A' or ch=='e' or ch=='E' or ch=='i' or ch=='I' or ch=='o' or ch=='O' or ch=='u' or ch=='U'):
-  print(ch,"Vowel")
-else:
   print(ch,"Consonant")
+else:
+  print(ch,"invalid")
